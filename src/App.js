1f4cc97565ab9d4fcca650c223db349134e4d2d5
Cameron Shimmin <cameronshimmin@Camerons-Mac-mini.local>
2020-09-12 14:17:24 -0700

    uuid update2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { v4 as uuidv4 } from 'uuid';
-uuidv4();
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
+uuidv4();
 function App() {
   return (
     <div>
